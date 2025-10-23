f166be41a445b613c680707fca47f728cb545967
Michal Bureš <michal.ezop@gmail.com>
2025-10-23 23:37:16 +0200

    Downgrade nixpkgs and Home Manager versions to 25.05 for compatibility with existing configurations

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -12,7 +12,7 @@
   # State version - determines config format and default behavior
   # WARNING: Do not change this after initial installation unless you know what you're doing
   # This is separate from the NixOS version in flake.nix
-  home.stateVersion = "25.11";
+  home.stateVersion = "25.05";
 
   # Enable Home Manager to manage itself
   programs.home-manager.enable = true;
